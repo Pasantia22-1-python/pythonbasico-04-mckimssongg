9e330b4c9d0a627ee5f3311c958d887c1bdaa86d
mckimssongg <86619053+mckimssongg@users.noreply.github.com>
2022-03-22 13:13:19 -0600

    add

---
+++ POO.py
@@ -0,0 +1,14 @@
+class Persona:
+    def init(self, name, age):
+        self.name = name
+        self.age = age
+
+    def diHola(self):
+        print(f'Hello my name is {self.name} and I am {self.age} years old')
+
+
+if name == 'main':
+    persona = Persona('Parce', 18)
+
+    print(f'Age: {self.age }')
+    persona.diHola()
